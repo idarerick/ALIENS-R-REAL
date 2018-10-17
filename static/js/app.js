34655861b0768694369eb66d19599e6d2c898414
idarerick <darrick.mail@gmail.com>
2018-10-16 20:06:48 -0700

    level 1 complete

--- static/js/app.js
+++ static/js/app.js
@@ -16,23 +16,34 @@ data.forEach(function(data) {
 
 
 var submit = d3.select("#filter-btn");
-
 submit.on("click", function() {
+	tbody.html("");
 	d3.event.preventDefault();
 	var date = d3.select("#datetime").property("value");
 	console.log(date);
-	if (date !== '') {
-		var filteredData = tableData.filter(data => data.datetime === date);
-		console.log(filteredData);
-		filteredData.forEach(data => {
-			var row = tbody.append("tr");
-			Object.entries(data).forEach(function([key, value]) {
-				console.log(key, value);
-				var cell = tbody.append("td");
-				cell.text(value);
-			})
+	var filteredData = tableData.filter(data => data.datetime === date);
+	console.log(filteredData);
+	filteredData.forEach(data => {
+		var row = tbody.append("tr");
+		Object.entries(data).forEach(function([key, value]) {
+			console.log(key, value);
+			var cell = tbody.append("td");
+			cell.text(value);
 		})
-	}
+	})
+
+
+	// else {
+	// 	data.forEach(function(data) {
+	// 		// console.log(data);
+	// 		var row = tbody.append("tr");
+	// 		Object.entries(data).forEach(function([key, value]) {
+	// 			console.log(key, value);
+	// 			var cell = tbody.append("td");
+	// 			cell.text(value);
+	// 		})
+	// 	});
+	// }
 });
 
 
